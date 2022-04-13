f5701282350aff34223d2934cc1e4fbbd74379ae
phdcoder <mestrealessandros@gmail.com>
2022-04-13 10:18:02 -0300

    aula 13-4 saving data.

--- controllers/BancoController.php
+++ controllers/BancoController.php
@@ -10,6 +10,17 @@ class BancoController extends Controller
 
     public function actionCreate(){
         $model = new Banco();
+        if ($this->request->isPost){
+            if( $model->load($this->request->post())) {
+                if ($model->save()) {
+                    return $this->redirect(['view',
+                        'id'=>$model->id] );
+                }
+            }
+        }
+        else {
+            $model->loadDefaultValues();
+        }
         return $this->render('create', [
             'model' => $model
         ]);
