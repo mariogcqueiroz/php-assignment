200d0029f8f3c3cc71dda66d152ae255286c78f0
phdcoder <mestrealessandros@gmail.com>
2023-03-06 09:00:08 -0300

    2023 - 1 commit!

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM dmstr/php-yii2:latest-nginx
-WORKDIR /app
-COPY . .
-RUN chmod -R 777 /app/web/assets 
-RUN chmod -R 777 /app/runtime
--- vendor/allankaio/giitester/model/form.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* @var $this yii\web\View */
-/* @var $form yii\widgets\ActiveForm */
-/* @var $generator yii\gii\generators\model\Generator */
-?>
-<?php
-    echo $form->field($generator, 'tableName');
-?>
