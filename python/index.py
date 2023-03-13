41175d86bcab9cd8c0655aef48f733045f7ab068
phdcoder <mestrealessandros@gmail.com>
2023-03-13 09:24:59 -0300

    aula 31/03

--- python/index.py
+++ python/index.py
@@ -1,5 +1,27 @@
+import wsgiref.simple_server
+import urllib.parse
+
 def app(environ, start_response):
-    data = b"Hello, Web!\n"
+    path = environ["PATH_INFO"]
+    method = environ["REQUEST_METHOD"]
+    data=b""
+    forms_data = []
+    if path == "/":
+        data = b"Hello, Web!\n"
+    if path == "/feedback":
+        if method == "POST":
+            input_obj = environ["wsgi.input"]
+            input_length = int(environ["CONTENT_LENGTH"])
+            body = input_obj.read(input_length).decode()
+            data = urllib.parse.parse_qs(body, keep_blank_values=True)
+            req = {
+                "name" : data["name"][0],
+                "email" : data["email"][0],
+                "content" : data["feedback"][0]
+            }
+            forms_data.append(req)
+            data = b"Your feedback submitted successfully."
+            
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
@@ -7,5 +29,9 @@ def app(environ, start_response):
     return iter([data])
 
 if __name__ == '__main__':
-    from gevent.pywsgi import WSGIServer
-    WSGIServer(('', 8000),app, spawn=None).serve_forever()
+    w_s = wsgiref.simple_server.make_server(
+        host="",
+        port=8000,
+        app=app
+    )
+    w_s.serve_forever()
