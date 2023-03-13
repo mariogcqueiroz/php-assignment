253d4e052e16eba6d7f90c992b64e810996fdd8a
phdcoder <mestrealessandros@gmail.com>
2023-03-13 10:06:08 -0300

    fix requirements.txt

--- php/web/index.php
+++ php/web/index.php
@@ -5,16 +5,15 @@ if ($_SERVER['REQUEST_URI'] === '/')
 	$data="Hello World";
 if ($_SERVER['REQUEST_URI'] === '/feedback') {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $name = $_POST['name'];
         $email = $_POST['email'];
         $message = $_POST['feedback'];
         $feedback = [
-            'name' => $name,
+            'name' => $_POST['name'],
             'email' => $email,
             'feedback' => $message,
         ];
         array_push($forms_data, $feedback);
-        $data = b"Your feedback submitted successfully.";
+        $data = "Your feedback submitted successfully.";
     }
 }
 echo $data;
