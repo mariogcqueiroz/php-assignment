9aa71dab5eaf66d1292aa194eb4dd0b0e244a097
phdcoder <mestrealessandros@gmail.com>
2023-03-27 11:03:06 -0300

    Php pronto para Enloquent

--- php/web/index.php
+++ php/web/index.php
@@ -1,8 +1,10 @@
 <?php
+include_once "../vendor/autoload.php";
+
 $data ="";
 $forms_data = [];
 if ($_SERVER['REQUEST_URI'] === '/')
-	$data="Hello World";
+    $data="Hello World";
 if ($_SERVER['REQUEST_URI'] === '/feedback') {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $feedback = [
@@ -13,12 +15,16 @@ if ($_SERVER['REQUEST_URI'] === '/feedback') {
         array_push($forms_data, $feedback);
         if ( strpos($feedback['email'],"@") ){
             $data = "Your feedback submitted successfully.".json_encode($forms_data);
-                $db = new \PDO("pgsql:host=db;dbname=site;port=5432",
-                    'app', 'app2022');
-                $sql = "insert into feedback(nome,email,feedback)
+            $db = new \PDO("pgsql:host=db;dbname=site;port=5432",
+                'app', 'app2022');
+            $sql = "insert into feedback(nome,email,feedback)
                   values( '".$feedback['name']."','".$feedback['email'].
-                    "','".$feedback['feedback']."') ";
-                $db->exec($sql);
+                "','".$feedback['feedback']."') ";
+            // insert into feedback(nome,email,feedback)
+            //                  values( '".$feedback['name']."','".$feedback['email'].
+            //                "','".$feedback['feedback']."')
+            //$feedback['feedback'] ??  blabal'); drop table feedback;select ('a
+            $db->exec($sql);
         }
         else {
             $error['email']="Email deve conter @";
