9a97b24205df54483935a9ccfea98114efec5d3c
phdcoder <mestrealessandros@gmail.com>
2023-03-28 16:17:59 -0300

    php atualizado com o Enloquent

--- php/web/index.php
+++ php/web/index.php
@@ -4,9 +4,20 @@ include_once "../vendor/autoload.php";
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+/**
+ * Model Feedback.
+ *
+ * @property int $id
+ * @property string $nome
+ * @property string $email
+ * @property string $feedback
+ **/
+class Feedback extends Model
+{
+    protected $table = 'feedback';
+    public $timestamps = false;
+}
 
-
-$capsule = new Capsule;
 $capsule = new Capsule;
 $capsule->addConnection([
     "driver" => "pgsql",
@@ -17,23 +28,21 @@ $capsule->addConnection([
 ]);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
-
-
 $data ="";
 $forms_data = [];
 if ($_SERVER['REQUEST_URI'] === '/')
     $data="Hello World";
 if ($_SERVER['REQUEST_URI'] === '/feedback') {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $feedback = [
-            'name' => $_POST['name'],
-            'email' => $_POST['email'],
-            'feedback' => $_POST['feedback'],
-        ];
+
+        $feedback= New Feedback;
+        $feedback->nome=$_POST['name'];
+        $feedback->email=$_POST['email'];
+        $feedback->feedback= $_POST['feedback'];
 
         if ( strpos($feedback['email'],"@") ){
             $data = "Your feedback submitted successfully.".json_encode($forms_data);
-
+            $feedback->save();
         }
         else {
             $error['email']="Email deve conter @";
