bdc4a85b083a6214f0f8b7e30e2af14936c29132
phdcoder <mestrealessandros@gmail.com>
2023-04-17 09:26:47 -0300

    config DEBUG

--- yii/config/db.php
+++ yii/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=db;dbname='.DB_DATABASE,
-    'username' => DB_USER,
-    'password' => DB_PASSWORD,
+    'dsn' => 'pgsql:host=db;dbname='.$_ENV['DB_DATABASE'],
+    'username' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD'],
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
