3c76ca42c28f56315997e37aced89f71719678dd
phdcoder <mestrealessandros@gmail.com>
2023-05-15 10:52:44 -0300

    API REST com Yii2

--- yii/config/web.php
+++ yii/config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'wgCzeUntlJ0GcP8dh6z09EPqBTQd97aT',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -63,14 +66,15 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-            ],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'feed'],
+            ]
         ],
-        */
+
     ],
     'modules'=>[
         'user-management' => [
---
+++ yii/controllers/FeedController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\controllers;
+
+
+class FeedController extends \yii\rest\ActiveController {
+
+    public $modelClass = 'app\models\Feedback';
+
+}
