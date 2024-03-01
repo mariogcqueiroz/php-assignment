75c145b799bbe017053dd806e805b76cb7ebd355
mariogcqueiroz <mariogcqueiroz@gmail.com>
2024-02-29 23:02:48 -0300

    Alterações no dockerfile, ainda falta revisar as conexões e conf

--- php/web/index.php
+++ php/web/index.php
@@ -1,2 +1,12 @@
 <?php
-echo "Hello World!";
+$path = $_SERVER['REQUEST_URI'];
+
+if ($path == "/app") {
+    echo "Hello, Web!\n";
+} elseif ($path == "/app/feedback" && $_SERVER['REQUEST_METHOD'] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $feedback = $_POST['feedback'];
+    echo "$name $email $feedback";
+}
+?>
